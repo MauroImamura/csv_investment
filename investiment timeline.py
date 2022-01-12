50f2ba87e9ad2a11c32e05c449a8db9f7e0a4a58
MauroT <mauro.imamura@outlook.com>
2022-01-11 22:58:43 -0300

    Functions for creating and writing on csv with investment info

--- investiment timeline.py
+++ investiment timeline.py
@@ -1,37 +1,51 @@
 import csv
+import os
+import datetime
 
-def add_header(inp_vector, file_name):
-      # write the header line into a csv file called file_name.
-      # if the file doesn't exists, a new one is created.
+def main():
+      # execute script
+      
+      print("Session initialized.")
+      print("...")
 
-      content = []
+      if 'statement.csv' not in os.listdir():
+            print("Creating file...")
+            create_statement()
 
-      f = open(file_name)
+      print("Recording file ready.")
+      print("...")
+      print("Use the invest function for new recordings.")
+      
+def create_statement():
+      # generates a new statement file for investment recording.
 
-      reader = csv.reader(f)
-      for row in reader:
-            content.append(row)
-
+      header = ['date','name','amount ($)','interest rate (%)']
+      
+      f = open('statement.csv', 'w',encoding = 'UTF8', newline = '')   
+      writer = csv.writer(f)
+      writer.writerow(header)
       f.close()
-      
-      f = open(file_name, 'w',encoding = 'UTF8', newline = '')
-      
+
+def invest():
+      # write a new line into a csv file called statement.
+
+      #ask for investmet details
+      inp_date = input('Investment date (dd/mm/yyyy): ')
+      name = input('Investment name or description: ')
+      amount = float(input('Amount ($): ').replace(',','.'))
+      rate = float(input('Interest rate (%): ').replace(',','.'))
+
+      #correct date format
+      [dd,mm,yyyy] = [int(inp_date[0:2]),int(inp_date[3:5]),int(inp_date[6:10])]
+      date = datetime.datetime(yyyy,mm,dd)
+
+      #list of investiment information
+      inp_vector = [date,name,amount,rate]
+
+      #write on statement file
+      f = open('statement.csv', 'a',encoding = 'UTF8', newline = '')
       writer = csv.writer(f)
       writer.writerow(inp_vector)
-      for i, row in enumerate(content):
-            if i > 0:
-                  writer.writerow(row)
-
       f.close()
 
-def add_line(inp_vector, file_name):
-      # write a new line into a csv file called file_name.
-      # if the file doesn't exists, a new one is created.
-      
-      f = open(file_name, 'a',encoding = 'UTF8', newline = '')
-
-      writer = csv.writer(f)
-
-      writer.writerow(inp_vector)
-
-      f.close()
+main()
