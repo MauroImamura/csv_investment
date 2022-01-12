02f84f54d2e96a689ba106e07a22508966625121
MauroT <mauro.imamura@outlook.com>
2022-01-12 00:05:12 -0300

    Date formating function and calculation function (WIP)

--- investiment timeline.py
+++ investiment timeline.py
@@ -1,6 +1,6 @@
 import csv
 import os
-import datetime
+from datetime import datetime
 
 def main():
       # execute script
@@ -29,23 +29,46 @@ def create_statement():
 def invest():
       # write a new line into a csv file called statement.
 
-      #ask for investmet details
+      # ask for investmet details
       inp_date = input('Investment date (dd/mm/yyyy): ')
       name = input('Investment name or description: ')
       amount = float(input('Amount ($): ').replace(',','.'))
       rate = float(input('Interest rate (%): ').replace(',','.'))
 
-      #correct date format
-      [dd,mm,yyyy] = [int(inp_date[0:2]),int(inp_date[3:5]),int(inp_date[6:10])]
-      date = datetime.datetime(yyyy,mm,dd)
+      # correct date format
+      date = convert_date(inp_date)
 
-      #list of investiment information
+      # list of investiment information
       inp_vector = [date,name,amount,rate]
 
-      #write on statement file
+      # write on statement file
       f = open('statement.csv', 'a',encoding = 'UTF8', newline = '')
       writer = csv.writer(f)
       writer.writerow(inp_vector)
       f.close()
+
+      print('Recording succeeded.')
+
+def calculate():
+      # calculate the total portfolio worth on the specified date
+
+      result = 0.0
+      
+      # set ref date type and format
+      inp_date = input('Reference date(dd/mm/yyyy): ')
+      date = convert_date(inp_date)
+
+      # read csv statement file and calculate investment worth
+      with open('statement.csv','r') as f:
+            reader = csv.reader(f)
+            next(reader)
+            for row in reader:
+                  print(row[0])
+
+def convert_date(str_date):
+      # set date type and format
+      [dd,mm,yyyy] = [int(str_date[0:2]),int(str_date[3:5]),int(str_date[6:10])]
+      date = datetime(yyyy,mm,dd)
+      return date
 
 main()
