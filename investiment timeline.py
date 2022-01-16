91f329002b2923f8d8f2758490d0ff1b012c1986
MauroT <mauro.imamura@outlook.com>
2022-01-16 00:18:52 -0300

    Numpy to calculate business days and urllib to get holidays list

--- investiment timeline.py
+++ investiment timeline.py
@@ -1,6 +1,8 @@
 import csv
 import os
 from datetime import datetime
+import urllib.request
+import numpy
 
 def main():
       # execute script
@@ -12,9 +14,15 @@ def main():
             print("Creating file...")
             create_statement()
 
+      print("Getting calendar information.")
+      print("...")
+      global holidays_list
+      holidays_list = get_holidays()
+
       print("Recording file ready.")
       print("...")
-      print("Use invest/calculate functions to record or evaluate investments.")
+      print("Use invest()/calculate() functions to record or evaluate investments.")
+      print("Use show() to see your statement.")
       
 def create_statement():
       # generates a new statement file for investment recording.
@@ -62,11 +70,32 @@ def calculate():
             for row in reader:
                   initial_date = datetime.strptime(row[0],'%d/%m/%Y')
                   if date_as_date > initial_date:
-                        t = (5/7)*(date_as_date - initial_date).days
+                        t = numpy.busday_count(initial_date.strftime('%Y-%m-%d'),date_as_date.strftime('%Y-%m-%d'))
                         current_amount = float(row[2])*(1+(float(row[3])/100))**(t/252)
                         print('Resulting amount of ', row[1], ': ', format(current_amount,'.2f'))
                         result += current_amount
 
       print('Resulting total amount: R$', format(result, '.2f'))
+
+def show():
+      #displays current statement on screen
+
+      with open('statement.csv','r') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                  print(row)
+
+def get_holidays():
+      #get the list of holidays and their dates on ANBIMA's library
+
+      hol_hist = {}
+      
+      for year in range(2001,2078):
+            link = 'https://www.anbima.com.br/feriados/fer_nacionais/{}.asp'.format(year)
+            with urllib.request.urlopen(link) as f:
+                  content = f.read()
+                  dec = content.decode()
+            hol_hist.update({year:dec})
+      return hol_hist
 
 main()
